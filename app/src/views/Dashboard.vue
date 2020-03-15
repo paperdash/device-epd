f4eae5a16443a09da72664c949363f6ecd72880f
Thomas Ballmann <mail@i-2xdream.de>
2020-03-15 09:46:11 +0100

    add weather reload button for dev

--- app/src/views/Dashboard.vue
+++ app/src/views/Dashboard.vue
@@ -1,28 +1,30 @@
 <template>
-    <v-container
-        fluid
-        grid-list-md
-        pa-2
-    >
-		<v-card
-			class="mx-auto my-12"
-			max-width="400"
-			elevation="24"
-			>
-			<v-img class="device-screen-image" :aspect-ratio="16/9" src="/fs/screen.bmp"></v-img>
-		</v-card>
-
-    </v-container>
+  <v-container fluid grid-list-md pa-2>
+    <v-card class="mx-auto my-12" max-width="400" elevation="24">
+      <!--<v-img class="device-screen-image" :aspect-ratio="16/9" src="/fs/screen.bmp"></v-img>-->
+    </v-card>
+    <v-btn class="ma-2" :loading="isLoading" color="secondary" @click="updateData">load weather data</v-btn>
+  </v-container>
 </template>
 
 <script>
-    //import api from '../api/core'
+import apiDevice from "@/api/device";
 
-    export default {
-		name: "Dashboard",
+export default {
+  data: () => ({
+    isLoading: false
+  }),
+  methods: {
+    updateData() {
+      this.isLoading = true;
+
+      apiDevice.updateWeather(() => {
+        this.isLoading = false;
+      });
     }
+  }
+};
 </script>
 
 <style scoped>
-
 </style>
