6060f5558409a4d9b182640b9ff524aaf0b66d16
Thomas Ballmann <mail@i-2xdream.de>
2020-04-29 21:15:36 +0200

    code clean up

--- src/imagePNG.cpp
+++ src/imagePNG.cpp
@@ -7,10 +7,6 @@ pngle_t *pngle;
 
 void on_draw(pngle_t *pngle, uint32_t x, uint32_t y, uint32_t w, uint32_t h, uint8_t rgba[4])
 {
-	uint8_t r = rgba[0]; // 0 - 255
-	uint8_t g = rgba[1]; // 0 - 255
-	uint8_t b = rgba[2]; // 0 - 255
-
 	ImageProcessPixel(x, y, rgba);
 }
 
@@ -20,8 +16,6 @@ void setupImagePNG()
 
 	pngle = pngle_new();
 	pngle_set_draw_callback(pngle, on_draw);
-
-	Serial.println("setupPNG done");
 }
 
 void pngOpenFramebuffer()
@@ -32,6 +26,8 @@ void pngOpenFramebuffer()
 
 void pngWriteFramebuffer(int offset, uint8_t bitmap[], int c)
 {
+	Serial.print(".");
+
 	int fed = pngle_feed(pngle, bitmap, c);
 	if (fed < 0)
 	{
