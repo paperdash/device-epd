31ed08308735d67079dd760babb2deb78a9c6cbb
Thomas Ballmann <mail@i-2xdream.de>
2020-06-21 21:42:52 +0200

    #11 use cloud api endpoint as image service

--- src/faceCalendar.cpp
+++ src/faceCalendar.cpp
@@ -22,7 +22,7 @@ void display_time();
 
 const char faceCalendarPicutreJson[] = "/calendarPhoto.json";
 const char faceCalendarPicture[] = "/calendarPhoto.jpg";
-const char faceCalendarPictureUrl[] = "http://sonic/paperdash/unsplash.php?&w=390&h=384&fm=jpg"; // TODO load from settings
+const char faceCalendarPictureUrl[] = "http://us-central1-paperdash-io.cloudfunctions.net/app/image/390x384.jpg"; // TODO load from settings
 
 unsigned long lastCalendarDataUpdate = 0;
 
