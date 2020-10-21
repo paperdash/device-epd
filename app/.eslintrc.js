fc523fb51cd72c012a73d4ab0333fecf861a7ad0
Thomas Ballmann <mail@i-2xdream.de>
2020-10-21 20:03:55 +0200

    lint.rc for vuetify added

---
+++ app/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  extends: 'vuetify',
+  // add your custom rules here
+  rules: {
+  },
+}
