94339ea1608ba1fbb366e9c1f0def9195c373e09
Thomas Ballmann <mail@i-2xdream.de>
2020-12-03 19:28:10 +0100

    correct scroll position after navigating

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -39,4 +39,11 @@ export default new VueRouter({
 
       { path: '*', redirect: '/' },
     ],
+    scrollBehavior (to, from, savedPosition) {
+      if (savedPosition) {
+        return savedPosition
+      } else {
+        return { x: 0, y: 0 }
+      }
+    },
 })
